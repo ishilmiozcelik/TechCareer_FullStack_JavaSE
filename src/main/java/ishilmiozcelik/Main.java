815c67e7180d753482121c87db0bcfdf250d30e5
ismail h. ozcelik <iozcelyk@gmail.com>
2023-08-17 19:39:28 +0300

    java

--- src/main/java/ishilmiozcelik/Main.java
+++ src/main/java/ishilmiozcelik/Main.java
@@ -9,6 +9,7 @@ public class Main {
 
         System.out.println("Bye bye Mars");
         System.out.println("Hi 2");
+        System.out.println("Hi 2");
 
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
