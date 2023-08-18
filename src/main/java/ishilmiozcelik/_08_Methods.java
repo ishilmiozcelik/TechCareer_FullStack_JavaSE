f1f25322e69a4920066b88a47c52bded85e569c6
ismail h. ozcelik <iozcelyk@gmail.com>
2023-08-18 15:14:10 +0300

    JavaSE

---
+++ src/main/java/ishilmiozcelik/_08_Methods.java
@@ -0,0 +1,18 @@
+package ishilmiozcelik;
+
+public class _08_Methods {
+
+
+
+    public void voidliParametresiz(){
+
+        System.out.println("voidli Parametresiz");
+
+    }
+
+    public void voidsizParametresiz(){
+
+        System.out.println("voidli Parametresiz");
+
+    }
+}
