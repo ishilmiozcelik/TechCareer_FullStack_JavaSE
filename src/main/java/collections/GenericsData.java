3b0d7b1e188ce2cf968cfde187b2f9182e4c48cd
ismail h. ozcelik <iozcelyk@gmail.com>
2023-08-20 19:52:48 +0300

    JavaSE|OOP|Interface|GenericsData

---
+++ src/main/java/collections/GenericsData.java
@@ -0,0 +1,24 @@
+package collections;
+
+public class GenericsData <T> {
+
+    private T adi;
+    public T getAdi() {
+        return adi;
+    }
+
+    public void setAdi(T adi) {
+        this.adi = adi;
+    }
+
+    public GenericsData(T adi) {
+        this.adi = adi;
+    }
+
+    public static void main(String[] args) {
+        GenericsData genericsData = new GenericsData();
+        genericsData.setAdi(34534);
+        System.out.println(genericsData.getAdi());
+    }
+
+}
